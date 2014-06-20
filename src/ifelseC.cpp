0662d9ee6164d13b8baff2f986c9accc35202a1f
Romain François <romain@r-enthusiasts.com>
2014-06-20 16:15:15 +0200

    simplify

--- src/ifelseC.cpp
+++ src/ifelseC.cpp
@@ -1,82 +1,29 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
+template <typename Vec>
+Vec safe_ifelse( LogicalVector test, Vec yes, Vec no){
+    int n_test = test.size(), n_yes = yes.size(), n_no = no.size();
+    if(n_test != n_yes | n_yes != n_no) stop("Different length of input vectors");
+    return ifelse(test, yes, no);
+}
+
 // [[Rcpp::export]]
 NumericVector ifelseCNum(LogicalVector test, NumericVector yes, NumericVector no) {
-  int n_test = test.size(), n_yes = yes.size(), n_no = no.size();
-  if(n_test != n_yes | n_yes != n_no) stop("Different length of input vectors");
-  
-  NumericVector out(n_test);
-  LogicalVector test_na = is_na(test);
-  for(int i = 0; i< n_test; i++){
-    if(test_na[i]){
-      out[i] = NA_REAL;
-    } else if(test[i]){
-      out[i] = yes[i];
-    } else {
-      out[i] = no[i];
-    }
-  }
-  
-  return out;
+  return safe_ifelse(test, yes, no) ;
 }
 
 // [[Rcpp::export]]
 CharacterVector ifelseCChar(LogicalVector test, CharacterVector yes, CharacterVector no) {
-  int n_test = test.size(), n_yes = yes.size(), n_no = no.size();
-  if(n_test != n_yes | n_yes != n_no) stop("Different length of input vectors");
-  
-  CharacterVector out(n_test);
-  LogicalVector test_na = is_na(test);
-  for(int i = 0; i< n_test; i++){
-    if(test_na[i]){
-      out[i] = NA_STRING;
-    } else if(test[i]){
-      out[i] = yes[i];
-    } else {
-      out[i] = no[i];
-    }
-  }
-  
-  return out;
+  return safe_ifelse(test, yes, no) ;
 }
 
 // [[Rcpp::export]]
 LogicalVector ifelseCLogic(LogicalVector test, LogicalVector yes, LogicalVector no) {
-  int n_test = test.size(), n_yes = yes.size(), n_no = no.size();
-  if(n_test != n_yes | n_yes != n_no) stop("Different length of input vectors");
-  
-  LogicalVector out(n_test);
-  LogicalVector test_na = is_na(test);
-  for(int i = 0; i< n_test; i++){
-    if(test_na[i]){
-      out[i] = NA_LOGICAL;
-    } else if(test[i]){
-      out[i] = yes[i];
-    } else {
-      out[i] = no[i];
-    }
-  }
-  
-  return out;
+  return safe_ifelse(test, yes, no) ;
 }
 
 // [[Rcpp::export]]
 IntegerVector ifelseCInt(LogicalVector test, IntegerVector yes, IntegerVector no) {
-  int n_test = test.size(), n_yes = yes.size(), n_no = no.size();
-  if(n_test != n_yes | n_yes != n_no) stop("Different length of input vectors");
-  
-  IntegerVector out(n_test);
-  LogicalVector test_na = is_na(test);
-  for(int i = 0; i< n_test; i++){
-    if(test_na[i]){
-      out[i] = NA_INTEGER;
-    } else if(test[i]){
-      out[i] = yes[i];
-    } else {
-      out[i] = no[i];
-    }
-  }
-  
-  return out;
+  return safe_ifelse(test, yes, no) ;
 }
