839e00e441c6fadcab043b5f142c55b87fa467ed
Romain François <romain@r-enthusiasts.com>
2014-06-20 16:35:41 +0200

    roll back the CharacterVector version because Rcpp is broken

--- src/ifelseC.cpp
+++ src/ifelseC.cpp
@@ -14,11 +14,6 @@ NumericVector ifelseCNum(LogicalVector test, NumericVector yes, NumericVector no
 }
 
 // [[Rcpp::export]]
-CharacterVector ifelseCChar(LogicalVector test, CharacterVector yes, CharacterVector no) {
-  return safe_ifelse(test, yes, no) ;
-}
-
-// [[Rcpp::export]]
 LogicalVector ifelseCLogic(LogicalVector test, LogicalVector yes, LogicalVector no) {
   return safe_ifelse(test, yes, no) ;
 }
@@ -26,4 +21,27 @@ LogicalVector ifelseCLogic(LogicalVector test, LogicalVector yes, LogicalVector 
 // [[Rcpp::export]]
 IntegerVector ifelseCInt(LogicalVector test, IntegerVector yes, IntegerVector no) {
   return safe_ifelse(test, yes, no) ;
+}
+
+// until Rcpp is fixed. 
+// https://github.com/RcppCore/Rcpp/pull/150
+
+// [[Rcpp::export]]
+CharacterVector ifelseCChar(LogicalVector test, CharacterVector yes, CharacterVector no) {
+  int n_test = test.size(), n_yes = yes.size(), n_no = no.size();
+  if(n_test != n_yes | n_yes != n_no) stop("Different length of input vectors");
+  
+  CharacterVector out(n_test);
+  LogicalVector test_na = is_na(test);
+  for(int i = 0; i< n_test; i++){
+    if(test_na[i]){
+      out[i] = NA_STRING;
+    } else if(test[i]){
+      out[i] = yes[i];
+    } else {
+      out[i] = no[i];
+    }
+  }
+  
+  return out;
 }
